3e4f706d4f34c586c7688e43096d2b480d7e6b2d
Lesya <lesyaaprok@gmail.com>
2024-07-22 23:47:52 +0300

    feat: add new categories, search items

--- yalta/xn--80afufmnqf3i.xn--c1acljkkgk2a2f.online/src/js/constants/categories/categories.js
+++ yalta/xn--80afufmnqf3i.xn--c1acljkkgk2a2f.online/src/js/constants/categories/categories.js
@@ -1,7 +1,9 @@
 import { beaches } from "./beaches.js";
+import { cities } from "./cities.js";
 import { observe } from "./observe.js";
 
 export const categories = {
+  Города: cities,
   Пляжи: beaches,
   "Смотровые площадки": observe,
 };
--- yalta/xn--80afufmnqf3i.xn--c1acljkkgk2a2f.online/src/js/search.js
+++ yalta/xn--80afufmnqf3i.xn--c1acljkkgk2a2f.online/src/js/search.js
@@ -19,9 +19,81 @@ const data = [
   },
   {
     id: 3,
+    name: "Гурзуф",
+    description: "населённые пункты",
+    url: "https://гурзуф.гидпокрыму.online",
+  },
+  {
+    id: 4,
+    name: "Форос",
+    description: "населённые пункты",
+    url: "https://форос.гидпокрыму.online",
+  },
+  {
+    id: 5,
+    name: "Кореиз",
+    description: "населённые пункты",
+    url: "https://кореиз.гидпокрыму.online",
+  },
+  {
+    id: 6,
+    name: "Олива",
+    description: "населённые пункты",
+    url: "https://олива.гидпокрыму.online",
+  },
+  {
+    id: 7,
+    name: "Алупка",
+    description: "населённые пункты",
+    url: "https://алупка.гидпокрыму.online",
+  },
+  {
+    id: 8,
+    name: "Оползневое",
+    description: "населённые пункты",
+    url: "https://оползневое.гидпокрыму.online",
+  },
+  {
+    id: 9,
+    name: "Голубой Залив",
+    description: "населённые пункты",
+    url: "https://голубойзаливсимеиз.гидпокрыму.online",
+  },
+  {
+    id: 10,
+    name: "Охотничье",
+    description: "населённые пункты",
+    url: "https://охотничье.гидпокрыму.online",
+  },
+  {
+    id: 11,
+    name: "Береговое",
+    description: "населённые пункты",
+    url: "https://береговое.гидпокрыму.online",
+  },
+  {
+    id: 12,
+    name: "Кацивели",
+    description: "населённые пункты",
+    url: "https://кацивели.гидпокрыму.online",
+  },
+  {
+    id: 13,
+    name: "Симеиз",
+    description: "населённые пункты",
+    url: "https://симеиз.гидпокрыму.online",
+  },
+  {
+    id: 14,
+    name: "Гаспра",
+    description: "населённые пункты",
+    url: "https://гаспра.гидпокрыму.online",
+  },
+  {
+    id: 15,
     name: "Симферополь",
     description: "города",
-    url: "",
+    url: "https://симферополь.гидпокрыму.online",
   },
 ];
 
